9af65bfa8811281b8cbd64196dc76cfc40632747
Yugi-Seong <77322860+Yugi-Seong@users.noreply.github.com>
2021-09-13 17:17:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function (req, res) {
-    res.send('hello world!!!');
+    res.send('hello!!');
 });
 
 app.get('/start', function (req, res) {
